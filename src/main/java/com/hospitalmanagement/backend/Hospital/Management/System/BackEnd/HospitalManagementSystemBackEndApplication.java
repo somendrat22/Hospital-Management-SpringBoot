d4d59f19acd82b81de0a514688e25e01e5edfcb5
somendra tiwari <somendrat22@outlook.com>
2023-10-23 20:33:01 +0530

    Adding initial code to github

---
+++ src/main/java/com/hospitalmanagement/backend/Hospital/Management/System/BackEnd/HospitalManagementSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.backend.Hospital.Management.System.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalmanagement/backend/Hospital/Management/System/BackEnd/HospitalManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.backend.Hospital.Management.System.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
