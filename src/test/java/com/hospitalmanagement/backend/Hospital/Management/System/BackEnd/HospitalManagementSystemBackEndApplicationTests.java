7950e58f50be3fb39e7cb9ab63c398644399d389
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2024-10-28 14:57:09 +0530

    Update HospitalManagementSystemBackEndApplicationTests.java

--- src/test/java/com/hospitalmanagement/backend/Hospital/Management/System/BackEnd/HospitalManagementSystemBackEndApplicationTests.java
+++ src/test/java/com/hospitalmanagement/backend/Hospital/Management/System/BackEnd/HospitalManagementSystemBackEndApplicationTests.java
@@ -16,39 +16,6 @@ import java.util.concurrent.TimeUnit;
 class HospitalManagementSystemBackEndApplicationTests {
 
 
-	@Autowired
-	DoctorService doctorService;
-
-	@BeforeEach
-	public void callingTest(){
-		System.out.println("Hey calling");
-	}
-
-	@AfterEach
-	public void afterCallingTest(){
-		System.out.println("Hey Called after test");
-	}
-
-	@Test
-	@Timeout(value=10000, unit = TimeUnit.MILLISECONDS)
-	public void checkGettingNullFromDoctorDBTest(){
-		int i  = 0;
-		while(i >= 0){
-			i++;
-		}
-		assert doctorService.getDoctorByDocID("xyz") == null : "getDoctorByDocId method is not working properly kindly look into that method";
-	}
-
-	@Test
-	public void checkAddPatientFunctionalityTest(){
-		System.out.println("Hey Executing test");
-		assert doctorService.getAllDocs().size() != 0 : "Hey There is no doctor in database so you can't add patient";
-	}
-
-
-
-
-
 
 
 }
